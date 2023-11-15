f2ad5330c1650db499b50baf517c2ea984c4e8f3
millarcharles <ch.cmillar@me.com>
2023-11-15 11:01:53 -0300

    Update streamlit_app.py
    Added emojis and avo prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard Boiled, Free-range Eggs')
+  
+streamlit.text('Omega 3 and Blueberry Oatmeal 🥣')
+streamlit.text('Kale, Spinach and Rocket Smoothie 🥗 ')
+streamlit.text('Hard Boiled, Free-range Eggs 🐔')
+streamlit.text('Avocado Toast 🥑🍞')
