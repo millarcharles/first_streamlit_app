e807bde3d230e0425fcd825f47f26e04e3400d5a
Carlos Millar <ch.cmillar@me.com>
2023-11-15 10:59:31 -0300

    added prompts

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard Boiled, Free-range Eggs')
