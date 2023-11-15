b7778514514b33705e72867e0543969cac259b28
millarcharles <ch.cmillar@me.com>
2023-11-15 11:14:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('Omega 3 and Blueberry Oatmeal 🥣')
 streamlit.text('Kale, Spinach and Rocket Smoothie 🥗 ')
 streamlit.text('Hard Boiled, Free-range Eggs 🐔')
 streamlit.text('Avocado Toast 🥑🍞')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
