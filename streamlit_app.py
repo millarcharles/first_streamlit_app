d5c42a8a6469fad963ef6c9fda9527967d77c048
millarcharles <ch.cmillar@me.com>
2023-11-21 21:34:00 -0300

    Update streamlit_app.py
    fetch all rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,9 @@ streamlit.text("The fruit load list contains:")
 
 
 ## fruit headers
-my_data_row = my_cur.fetchone()
+my_data_rows =  my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_rows)
+
+
 
