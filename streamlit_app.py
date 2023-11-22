4ad641ef81783b41889116584def6a1be29f457a
millarcharles <ch.cmillar@me.com>
2023-11-22 09:51:23 -0300

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
     fruityvice_normalized =  pd.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
-except URLerror as e:
+except URLError as e:
   streamlit.eror()
   
 
